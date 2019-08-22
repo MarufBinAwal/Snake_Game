a4910bc5074abbf95c49637eb307c58df67529fe
Theodore Kimana <kimana.theodore@gmail.com>
2019-08-22 16:21:30 -0500

    d

--- Snake_Game Back_End/app/controllers/api/v1/scores_controller.rb
+++ Snake_Game Back_End/app/controllers/api/v1/scores_controller.rb
@@ -3,5 +3,22 @@ class Api::V1::ScoresController < ApplicationController
       @scores = Score.all
       render json: @scores, only:[:id], include: :user 
     end
+
+    def create 
+     @scores = Score.new(score_params)
+     if @score.save
+        render json: @score, only: [:user_id, :s], include: :user
+     else
+
+     end
+    private
+    def score_params
+      params.require(:score).permit(:s, :user_id)
+  end 
+  def update
+    @score
+  end
+ end
+end  
     
-end
+
--- Snake_Game Back_End/app/controllers/api/v1/users_controller.rb
+++ Snake_Game Back_End/app/controllers/api/v1/users_controller.rb
@@ -3,4 +3,10 @@ class Api::V1::UsersController < ApplicationController
         @users = User.all
         render json: @users, only: [:id, :name], include: :scores    
     end 
+
+    # def show 
+    # @user = User.find(params[:id])
+    # end 
+
+    
 end
